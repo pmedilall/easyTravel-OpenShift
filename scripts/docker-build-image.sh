69d7b414bfb9e490cb495d8f01cd0d697198a3e0
Martin Etmajer <m.etmajer@gmail.com>
2016-05-24 00:12:07 +0200

    Fix Docker image tags.

--- scripts/docker-build-image.sh
+++ scripts/docker-build-image.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
+
 DOCKER_FILE=${DOCKER_FILE:-Dockerfile}
+DOCKER_IMAGE_SUFFIX=openshift
 DOCKER_IMAGE_NAME=${DOCKER_IMAGE_NAME}
-DOCKER_IMAGE_VERSION=${DOCKER_IMAGE_VERSION}-openshift
+DOCKER_IMAGE_VERSION=${DOCKER_IMAGE_VERSION}
 
 if [ -z "${DOCKER_IMAGE_NAME}" ]; then
   echo "Error: DOCKER_IMAGE_NAME is undefined"; exit 1
@@ -11,10 +13,10 @@ if [ -z "${DOCKER_IMAGE_VERSION}" ]; then
   echo "Error: DOCKER_IMAGE_VERSION is undefined"; exit 1
 fi
 
-echo "Building Docker image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"
-docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} -f ${DOCKER_FILE} .
+echo "Building Docker image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}-${DOCKER_IMAGE_SUFFIX}"
+docker build -t ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}-${DOCKER_IMAGE_SUFFIX} -f ${DOCKER_FILE} .
 if [ $? -ne 0 ]; then
-  echo "Error: could not build image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}"; exit 1
+  echo "Error: could not build image: ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}-${DOCKER_IMAGE_SUFFIX}"; exit 1
 fi 
 
-docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION} ${DOCKER_IMAGE_NAME}
+docker tag ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_VERSION}-${DOCKER_IMAGE_SUFFIX} ${DOCKER_IMAGE_NAME}:${DOCKER_IMAGE_SUFFIX}
