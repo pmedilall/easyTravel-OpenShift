33bdbd38a944d07e3d42bd2622ddca9a0e178976
Martin Etmajer <m.etmajer@gmail.com>
2016-05-24 13:05:05 +0200

    Update deployment script and fix wiring.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
+
+set -e
+
 . ./config/os-settings.sh
 
 oc login https://${OS_MASTER_IP}:8443 --username=admin --password=admin
 
+oc new-project easytravel || true
 oc new-app dynatrace/easytravel-mongodb:openshift
-oc new-app dynatrace/easytravel-backend:openshift --env ET_DATABASE_LOCATION=easytravel-mongodb:27017 --env CATALINA_OPTS=-Dconfig.mongoDbUser=
-oc new-app dynatrace/easytravel-frontend:openshift --env ET_BACKEND_URL=easytravel-backend:8080
+oc new-app dynatrace/easytravel-backend:openshift --env ET_DATABASE_LOCATION=easytravel-mongodb:27017
+oc new-app dynatrace/easytravel-frontend:openshift --env ET_BACKEND_URL='http://easytravel-backend:8080'
