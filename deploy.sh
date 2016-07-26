72601c9979f3e58e8d2175ed65968eb3bd606b01
Martin Etmajer <m.etmajer@gmail.com>
2016-07-26 15:09:57 +0200

    Fix template name.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 . ./config/os-settings.sh
 
 oc login https://${OS_MASTER_IP}:8443 --username=admin --password=admin
-oc create -f templates/dynatrace-easytravel.yml
+oc create -f templates/easytravel.yml
 oc new-app dynatrace-easytravel
