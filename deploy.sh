59767738badd71e05414a9b53649e0f6bd3abaa1
Martin Etmajer <m.etmajer@gmail.com>
2016-07-26 20:38:08 +0200

    Use short options.

--- deploy.sh
+++ deploy.sh
@@ -4,6 +4,6 @@ set -e
 
 . ./config/os-settings.sh
 
-oc login https://${OS_MASTER_IP}:8443 --username=admin --password=admin
+oc login https://${OS_MASTER_IP}:8443 -u admin -p admin
 oc create -f templates/easytravel.yml
 oc new-app dynatrace-easytravel
