b6c82b10dbe588fe8029f25c0ac36bfd3bd82ec6
Martin Etmajer <m.etmajer@gmail.com>
2016-07-26 22:39:58 +0200

    Deploy with loadgen per default.

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ set -e
 . ./config/os-settings.sh
 
 oc login https://${OS_MASTER_IP}:8443 -u admin -p admin
-oc create -f templates/easytravel.yml
+oc create -f templates/easytravel-with-loadgen.yml
 oc new-app dynatrace-easytravel
